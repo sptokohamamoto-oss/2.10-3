38be5440b92f13f6425f9acdd461d8b2cea3c909
冬桜子 濱元 <toko0124615@icloud.com>
2026-01-07 14:09:44 +0900

    bb

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int intTest = 1;
+
+        string StringTest = "01";
+
+        Console.WriteLine($"String{intTest}");
+
+
+        int convertendInt = int.Parse(StringTest);
+        int result = convertendInt + 1;
+
+        Console.WriteLine(result);
+
+    }
+
+}
